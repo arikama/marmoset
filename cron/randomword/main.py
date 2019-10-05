51105dd186af77838ad2210fea747f5473e9ba19
cglotr <amir.ariffin.920404@gmail.com>
2019-10-05 16:59:53 +0800

    iterate to make more frequent

--- cron/randomword/main.py
+++ cron/randomword/main.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get("https://randomword.com")
-soup = BeautifulSoup(r.text, features="html.parser")
-word = soup.find(id="random_word").get_text()
-json = {
-    "payload": {
-        "word": word
-    }
-}
-p = requests.post("http://service-cluster-ip-server/words", json=json)
-print(r.status_code, json)
+class Scraper():
+
+    def post(self):
+        word = self._scrapeWord()
+        json = {
+            "payload": {
+                "word": word
+            }
+        }
+        p = requests.post("http://service-cluster-ip-server/words", json=json)
+        return p.status_code, json
+
+    def _scrapeWord(self):
+        r = requests.get("https://randomword.com")
+        soup = BeautifulSoup(r.text, features="html.parser")
+        word = soup.find(id="random_word").get_text()
+        return word
+
+def main():
+    scraper = Scraper()
+    for i in range(10):
+        status, json = scraper.post()
+        print(i, status, json)
+
+main()
