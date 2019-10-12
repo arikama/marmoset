6c141312cb9ea7e6d7dc358d7fcdb55781259c08
cglotr <amir.ariffin.920404@gmail.com>
2019-10-12 08:49:43 +0800

    expose client

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ then
   exit 1
 fi
 
+docker build -t $1/marmoset-client -t $1/marmoset-client:$(git rev-parse HEAD) ./client
 docker build -t $1/marmoset-cron-job-randomword -t $1/marmoset-cron-job-randomword:$(git rev-parse HEAD) ./cron/randomword
 docker build -t $1/marmoset-server -t $1/marmoset-server:$(git rev-parse HEAD) ./server
 
+docker push $1/marmoset-client:latest
+docker push $1/marmoset-client:$(git rev-parse HEAD)
 docker push $1/marmoset-cron-job-randomword:latest
 docker push $1/marmoset-cron-job-randomword:$(git rev-parse HEAD)
 docker push $1/marmoset-server:latest
@@ -15,4 +18,5 @@ docker push $1/marmoset-server:$(git rev-parse HEAD)
 kubectl apply -f ./kubernetes
 
 kubectl set image cronjobs/cron-job-randomword cron-randomword=$1/marmoset-cron-job-randomword:$(git rev-parse HEAD)
+kubectl set image deployments/deployment-client client=$1/marmoset-client:$(git rev-parse HEAD)
 kubectl set image deployments/deployment-server server=$1/marmoset-server:$(git rev-parse HEAD)
