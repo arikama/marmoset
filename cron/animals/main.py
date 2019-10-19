156c97813cc61222687cfdd98e148d23d3d290ab
cglotr <amir.ariffin.920404@gmail.com>
2019-10-19 20:16:05 +0800

    animal scraper cron

--- cron/animals/main.py
+++ cron/animals/main.py
@@ -1,10 +1,38 @@
 from bs4 import BeautifulSoup
+import re
 import requests
+
+class Regex():
+
+    p = re.compile("rand_medium\\\"'>([a-z]+)")
+
+    def all(self, s):
+        animals = []
+        matches = self.p.finditer(s)
+        for match in matches:
+            animal = match.group(1)
+            animals.append(animal)
+        return animals
 
 class Scraper():
 
-    def post(self):
-        self._scrapeAnimals()
+    def __init__(self, parser):
+        self.parser = parser
+
+    def start(self):
+        animals = self._scrapeAnimals()
+        for animal in animals:
+            print('animal', animal)
+            self._post(animal)
+
+    def _post(self, animal):
+        json = {
+            "payload": {
+                "word": animal
+            }
+        }
+        p = requests.post("http://service-cluster-ip-server/words", json=json)
+        return p.status_code, json
 
     def _scrapeAnimals(self):
         json = {
@@ -13,10 +41,12 @@ class Scraper():
         }
         r = requests.get('http://service-cluster-ip-webscraper:8080', json=json)
         soup = BeautifulSoup(r.text, features="html.parser")
-        print('soup', soup)
+        animals = self.parser.all(str(soup))
+        return animals
 
 def main():
-    scraper = Scraper()
-    scraper.post()
+    parser = Regex()
+    scraper = Scraper(parser)
+    scraper.start()
 
 main()
