166006404568d17f505caa4230051c43a5ea5c32
cglotr <amir.ariffin.920404@gmail.com>
2019-10-19 18:56:42 +0800

    expect ownership to arikama image repo

--- README.md
+++ README.md
@@ -7,19 +7,22 @@ It's a clone to this [app](https://randomword.com/) and we are somehow way faste
 ## Deployment
 
 1. Make sure you're connected the cluster
-   
+
    `kubectl cluster-info`
 
 2. Create a Secret to store your MySQL database password
-   
+
    `kubectl create secret generic secret-mysql-password --from-literal=MYSQL_PASSWORD=$MYSQL_PASSWORD`
 
 3. Deploy
-   
-   `./deploy.sh $DOCKER_USERNAME`
+
+   `./deploy.sh`
+
+   **Note:** If you don't have access to Arikama's image repository, you'll have to rename it to something that
+   you own
 
 4. Perform database migration
-   
+
    `./migrate.sh $MYSQL_PASSWORD`
 
 5. [Install](https://kubernetes.github.io/ingress-nginx/deploy/) NGINX Ingress Controller
--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,20 @@
-if [ -z "$1" ]
-then
-  echo "error: docker username not specified"
-  exit 1
-fi
+docker build -t arikama/marmoset-client -t arikama/marmoset-client:$(git rev-parse HEAD) ./client
+docker build -t arikama/marmoset-cron-job-animals -t arikama/marmoset-cron-job-animals:$(git rev-parse HEAD) ./cron/animals
+docker build -t arikama/marmoset-cron-job-randomword -t arikama/marmoset-cron-job-randomword:$(git rev-parse HEAD) ./cron/randomword
+docker build -t arikama/marmoset-server -t arikama/marmoset-server:$(git rev-parse HEAD) ./server
 
-docker build -t $1/marmoset-client -t $1/marmoset-client:$(git rev-parse HEAD) ./client
-docker build -t $1/marmoset-cron-job-animals -t $1/marmoset-cron-job-animals:$(git rev-parse HEAD) ./cron/animals
-docker build -t $1/marmoset-cron-job-randomword -t $1/marmoset-cron-job-randomword:$(git rev-parse HEAD) ./cron/randomword
-docker build -t $1/marmoset-server -t $1/marmoset-server:$(git rev-parse HEAD) ./server
-
-docker push $1/marmoset-client:latest
-docker push $1/marmoset-client:$(git rev-parse HEAD)
-docker push $1/marmoset-cron-job-animals:latest
-docker push $1/marmoset-cron-job-animals:$(git rev-parse HEAD)
-docker push $1/marmoset-cron-job-randomword:latest
-docker push $1/marmoset-cron-job-randomword:$(git rev-parse HEAD)
-docker push $1/marmoset-server:latest
-docker push $1/marmoset-server:$(git rev-parse HEAD)
+docker push arikama/marmoset-client:latest
+docker push arikama/marmoset-client:$(git rev-parse HEAD)
+docker push arikama/marmoset-cron-job-animals:latest
+docker push arikama/marmoset-cron-job-animals:$(git rev-parse HEAD)
+docker push arikama/marmoset-cron-job-randomword:latest
+docker push arikama/marmoset-cron-job-randomword:$(git rev-parse HEAD)
+docker push arikama/marmoset-server:latest
+docker push arikama/marmoset-server:$(git rev-parse HEAD)
 
 kubectl apply -f ./kubernetes
 
-kubectl set image cronjobs/cron-job-animals cron-animals=$1/marmoset-cron-job-animals:$(git rev-parse HEAD)
-kubectl set image cronjobs/cron-job-randomword cron-randomword=$1/marmoset-cron-job-randomword:$(git rev-parse HEAD)
-kubectl set image deployments/deployment-client client=$1/marmoset-client:$(git rev-parse HEAD)
-kubectl set image deployments/deployment-server server=$1/marmoset-server:$(git rev-parse HEAD)
+kubectl set image cronjobs/cron-job-animals cron-animals=arikama/marmoset-cron-job-animals:$(git rev-parse HEAD)
+kubectl set image cronjobs/cron-job-randomword cron-randomword=arikama/marmoset-cron-job-randomword:$(git rev-parse HEAD)
+kubectl set image deployments/deployment-client client=arikama/marmoset-client:$(git rev-parse HEAD)
+kubectl set image deployments/deployment-server server=arikama/marmoset-server:$(git rev-parse HEAD)
